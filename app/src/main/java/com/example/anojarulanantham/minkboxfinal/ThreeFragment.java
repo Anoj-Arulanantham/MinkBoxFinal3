6bc2a99c74effa996c601147b231b810ed99260f
Anoj Arulanantham <aarulana@uwaterloo.ca>
2017-03-14 21:02:41 -0400

    committing a lot of work

--- app/src/main/java/com/example/anojarulanantham/minkboxfinal/ThreeFragment.java
+++ app/src/main/java/com/example/anojarulanantham/minkboxfinal/ThreeFragment.java
@@ -24,6 +24,7 @@ public class ThreeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_three, container, false);
+        System.out.println("HELLO");
         return v;
     }
 
